5095a53551ede47028457f2fa3456ac1fb0a1deb
asindrawan01 <93034587+asindrawan01@users.noreply.github.com>
2021-11-04 00:34:52 +0700

    Update query.sql

--- sql/query.sql
+++ sql/query.sql
@@ -1,2 +1,2 @@
 1. select * from(select * from people where age is not null) as listofpeople order by age asc limit 3;
-2. select count(name) as 'number of people' from people where adress = 'Jakarta' and age = null;
+2. select count(*)-count(age) as 'number of people' from people where adress = "Jakarta";
